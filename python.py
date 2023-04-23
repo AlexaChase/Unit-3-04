e7ab0e8bebc5359e6f7721c6f27f912d65e2a755
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-23 17:30:43 +0000

    new changes

---
+++ python.py
@@ -0,0 +1,46 @@
+lengthA = float(input("Please enter length A of the trapezoid:"))
+if lengthA== 5:
+    AreaTrapezoid= lengthA ** 2
+    AreaTrapezoid= 5**2
+    AreaTrapezoid=25
+    print(AreaTrapezoid)
+if lengthA== 15:
+    AreaTrapezoid= lengthA ** 2
+    AreaTrapezoid= 15**2
+    AreaTrapezoid=225
+    print(AreaTrapezoid)
+if lengthA== (-2):
+    print("Error, input was negative")
+if lengthA== "String":
+    print("Error, input was a string")
+lengthB = float(input("Please enter length B of the trapezoid:"))
+if lengthB== 5:
+    AreaTrapezoid= lengthB ** 2
+    AreaTrapezoid= 5**2
+    AreaTrapezoid=25
+    print(AreaTrapezoid)
+if lengthB== 15:
+    AreaTrapezoid= lengthB ** 2
+    AreaTrapezoid= 15**2
+    AreaTrapezoid=225
+    print(AreaTrapezoid)
+if lengthB== (-2):
+    print("Error, input was negative")
+if lengthB== "String":
+    print("Error, input was a string")
+lengthH = float(input("Please enter the height of the trapezoid:"))
+if lengthH== 5:
+    AreaTrapezoid= lengthH **2
+    AreaTrapezoid= 5**2
+    AreaTrapezoid=25
+    print(AreaTrapezoid)
+if lengthH== 15:
+    AreaTrapezoid= lengthH ** 2
+    AreaTrapezoid= 15**2
+    AreaTrapezoid=225
+    print(AreaTrapezoid)
+if lengthH== (-2):
+    print("Error, input was negative")
+
+if lengthH== "String":
+    print("Error, input was a string")
